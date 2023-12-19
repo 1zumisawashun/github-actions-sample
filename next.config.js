787562344842b88d228be0ea0bc502686d82de1d
1zumisawashun <shun.1zumisawa@gmail.com>
2023-12-19 10:40:41 +0900

    feat:add-github-actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/gha-standardization-sample"
+      : "./",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
