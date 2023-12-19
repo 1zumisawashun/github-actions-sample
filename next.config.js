8feab8e32f1c9d062caf53f3b48b833494a385dc
1zumisawashun <shun.1zumisawa@gmail.com>
2023-12-19 14:46:57 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "/gha-standardization-sample"
-      : "./",
+  // reactStrictMode: true,
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/gha-standardization-sample"
+  //     : "./",
 };
 
 module.exports = nextConfig;
