5f217ce94372f08a5901cae60090c8922562d138
1zumisawashun <shun.1zumisawa@gmail.com>
2023-12-20 21:01:42 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-// @ts-check
-
 /**
  * @type {import('next').NextConfig}
  **/
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
+
 const nextConfig = {
   /**
    * Enable static exports for the App Router.
@@ -29,4 +33,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
