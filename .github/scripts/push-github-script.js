ecb5d55328339cd4e9588ca434f17faf546a94e3
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-20 11:44:10 +0900

    fix

--- .github/scripts/push-github-script.js
+++ .github/scripts/push-github-script.js
@@ -1,5 +1,5 @@
 // @ts-check
-/** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+// /** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 export default async ({ core, context }) => {
   core.debug("Running something at the moment");
   return context.actor;
