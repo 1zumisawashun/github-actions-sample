6b4318e250232ee7c75ba8555f63d346f8b0dfab
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-21 00:53:06 +0900

    fix

--- .github/scripts/create-comment.js
+++ .github/scripts/create-comment.js
@@ -4,6 +4,26 @@ module.exports = async ({ github, context, core }) => {
   // リポジトリ情報を取得
   const { repo, owner } = context.repo;
 
+  const comments = await github.rest.issues.listComments({
+    issue_number,
+    owner,
+    repo,
+  });
+
+  const comment = comments.data.find((comment) =>
+    comment.body.startsWith("👋 Thanks for reporting!")
+  );
+
+  if (comment) {
+    await github.rest.issues.updateComment({
+      owner,
+      repo,
+      comment_id: comment.id,
+      body: "👋 Thanks for reporting! updated",
+    });
+    return;
+  }
+
   await github.rest.issues.createComment({
     issue_number,
     owner,
