503be6d33a77f68c81cf5943ad28b19c985199ee
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-20 16:03:43 +0900

    fix

--- .github/scripts/post-comment.js
+++ .github/scripts/post-comment.js
@@ -1,8 +1,8 @@
 module.exports = async ({ github, context, core }) => {
-  const res = github.issues.createComment({
+  github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: process.env.MESSAGE,
+    body: "👋 Thanks for reporting!",
   });
 };
