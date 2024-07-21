7baa995608c1aef0b37e3d814d431b56cd6a32cf
1zumisawashun <shun.1zumisawa@gmail.com>
2024-07-21 14:46:11 +0900

    fix

--- .github/scripts/create-comment.js
+++ .github/scripts/create-comment.js
@@ -1,3 +1,17 @@
+/** @see https://zenn.dev/yy616/articles/433f1496f69b30 */
+
+/**
+ * @typedef {import("@actions/github").context } Context
+ * @typedef {import("@actions/core")} Core
+ * @typedef {ReturnType<import("@actions/github")["getOctokit"]>} GitHub
+ */
+
+/**
+ * @param {Object} options - The options object.
+ * @param {GitHub} options.github - The GitHub object.
+ * @param {Context} options.context - The context object.
+ * @param {Core} options.core - The core object.
+ */
 module.exports = async ({ github, context, core }) => {
   // PR番号を取得
   const issue_number = context.issue.number;
